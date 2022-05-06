edc1004404760ac3cb0978d99860c8d756b76770
Renato Cardoso <renato-r-cardoso@alticelabs.com>
2022-05-06 16:43:27 +0100

    sec

---
+++ demo/src/test/java/com/demo/teste.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class teste {
+    @Test
+    public void test() {
+        boolean val = true;
+        assertTrue(val);
+    }
+}
