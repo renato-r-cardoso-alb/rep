a5c1a2da90ee61d004f22c5ad2f7d07084449eac
Renato Cardoso <renato-r-cardoso@alticelabs.com>
2022-05-10 15:04:00 +0100

    commit

--- demo/src/main/java/com/demo/App.java
+++ demo/src/main/java/com/demo/App.java
@@ -1,18 +1,46 @@
 package com.demo;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import com.sun.net.httpserver.*;
+
+public class App {
+       
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8500), 0);
+        HttpContext context = server.createContext("/example");
+        context.setHandler(App::handleRequest);
+        server.start();
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+    private static void handleRequest(HttpExchange exchange) throws IOException {
+        URI requestURI = exchange.getRequestURI();
+        printRequestInfo(exchange);
+        String response = "This is the response at " + requestURI;
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+    
+    private static void printRequestInfo(HttpExchange exchange) {
+        System.out.println("-- headers --");
+        Headers requestHeaders = exchange.getRequestHeaders();
+        requestHeaders.entrySet().forEach(System.out::println);
+  
+        System.out.println("-- principle --");
+        HttpPrincipal principal = exchange.getPrincipal();
+        System.out.println(principal);
+  
+        System.out.println("-- HTTP method --");
+        String requestMethod = exchange.getRequestMethod();
+        System.out.println(requestMethod);
+  
+        System.out.println("-- query --");
+        URI requestURI = exchange.getRequestURI();
+        String query = requestURI.getQuery();
+        System.out.println(query);
     }
 }
